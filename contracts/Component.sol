7d02cb8e2624eb41e9e585594a7307d961967c1c
Jan Pressler <jan.pressler@mailbox.org>
2021-10-27 15:53:37 +0200

    [WIP] Components /w ingredients, [WIP] tests

--- contracts/Component.sol
+++ contracts/Component.sol
@@ -1,12 +1,12 @@
-// contracts/GameItem.sol
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 
-contract Component is ERC721URIStorage {
+contract Component is ERC721URIStorage, IERC721Receiver {
+    
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     
@@ -17,28 +17,40 @@ contract Component is ERC721URIStorage {
     address owner;
     uint256 curToken = 0;
     
+    mapping (address => uint256[]) internal ownedTokens;
+    
     
     constructor(string memory componentName, string memory componentShort) ERC721(componentName, componentShort) {
         owner = msg.sender;
     }
     
-    // Call when initializing component that has ingredients
-    function setIngredients(address[] memory _ingredients, uint256[] memory _ingredientCount) public {
-        ingredients = _ingredients;
-        ingredientCount = _ingredientCount;
+
+    function addIngredient(address _ingredientAdr, uint256 _amount) public {
+        require(msg.sender == owner);
+        
         hasIngredients = true;
+        ingredients.push(_ingredientAdr);
+        ingredientCount.push(_amount);
     }
 
-    function mintComponent(string memory tokenURI) internal returns (uint256) {
-        if(!hasIngredients) {
-            _tokenIds.increment();
-    
-            uint256 newItemId = _tokenIds.current();
-            _mint(owner, newItemId);
-            _setTokenURI(newItemId, tokenURI);
-    
-            return newItemId;
+    function mintComponent(string memory tokenURI) public payable returns (uint256) {
+        require(msg.sender == owner);
+        
+        if (hasIngredients) {
+            for(uint256 i=0; i < ingredients.length; i++) {
+                Component(ingredients[i]).burn(ownedTokens[ingredients[i]][0]);    
+            }
+            // TODO revert if burning all tokens doesn't succeed
+            
         }
+        
+        _tokenIds.increment();
+    
+        uint256 newItemId = _tokenIds.current();
+        _mint(owner, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+    
+        return newItemId;
     }
     
     uint256 batchNo = 0;
@@ -46,6 +58,8 @@ contract Component is ERC721URIStorage {
     mapping (uint256 => uint256) internal batchSize;
     
     function mintBatch(uint256 amount) public returns (uint256) {
+        require(msg.sender == owner);
+        
         batchNo++;
         uint256 cID;
         
@@ -56,44 +70,52 @@ contract Component is ERC721URIStorage {
         
         batchSize[batchNo] = amount;
         
+        //return _tokenIds.current();
         return batchNo;
     }
     
-    mapping (uint256 => address) internal batchOwner;
+    mapping (uint256 => address) public batchOwner;
     
 
-    /*
     function transferBatch(address _to, uint256 _batchNo) public {
-        // TODO loop through batch
         for(uint256 i=0; i < _batchNo; i++) {
             
         }
         
-        safeTransferFrom(owner, _to, _tokenIds.current());
+        //safeTransferFrom(owner, _to, _tokenIds.current());
         batchOwner[_batchNo] = _to;
     }
+
     
-    function burnBatch(uint256 _batchNo) public {
+    function transferToken(address to) internal returns (uint256) {
+        require(msg.sender == owner);
         
-    }
-    */
-    
-    function transferToken(address to) public {
-        safeTransferFrom(owner, to, curToken);
         curToken++;
+        safeTransferFrom(owner, to, curToken);
+        //ownedTokens[to].push(curToken);
+        
+        return curToken;
     }
     
-    function transferTokens(address to, int256 amount) public {
-        for(int i=0; i < amount; i++) {
+    function transferTokens(address to, uint256 amount) public {
+        require(msg.sender == owner);
+        
+        for(uint256 i=0; i < amount; i++) {
             transferToken(to);
         }
     }
     
     // from ERC721Burnable
-    function burn(uint256 tokenId) public {
+    function burn(uint256 tokenId) public payable {
         //solhint-disable-next-line max-line-length
         require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721Burnable: caller is not owner nor approved");
         _burn(tokenId);
+    }
+    
+    
+    function onERC721Received(address from, address, uint256 tID, bytes memory) public virtual override returns (bytes4) {
+        ownedTokens[from].push(tID);
+        return this.onERC721Received.selector;
     }
     
 }
--- tests/Component_test.sol
+++ tests/Component_test.sol
@@ -1,5 +1,3 @@
-// SPDX-License-Identifier: GPL-3.0
-
 pragma solidity ^0.8.0;
 import "remix_tests.sol"; // this import is automatically injected by Remix.
 import "../contracts/Component.sol";
@@ -8,7 +6,6 @@ contract ComponentTest {
    
     bytes32[] proposalNames;
    
-    
     Component glue;
     Component wood;
     Component gluedWood;
@@ -16,10 +13,25 @@ contract ComponentTest {
     function beforeAll () public {
         glue = new Component("Glue", "GLU");
         wood = new Component("Wooden log", "LOG");
+        
+        gluedWood = new Component("Glued Wood", "GLW");
+        gluedWood.addIngredient(address(glue), 2);
+        gluedWood.addIngredient(address(wood), 3);
     }
     
     function mintBasicComponent () public {
-        glue.mintBatch(30);    
+        glue.mintBatch(2);
+        wood.mintBatch(3);
     }
+    
+    function transferIngredientsToSawmill () public {
+        glue.transferTokens(address(gluedWood), 2);
+        wood.transferTokens(address(gluedWood), 3);
+    }
+    
+    function mintComponentWithIngredients() public {
+        gluedWood.mintComponent("FirstGluedWood");
+    }
+    
 }
 
